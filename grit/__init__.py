aae5a53468887e740cc1fd92659ec8552dccd7d8
johnowusuduah <johnoduah@yahoo.co.uk>
2023-11-22 15:37:35 -0500

    initial commit of DivNET code

---
+++ grit/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+from .modeling.meta_arch import grit
+from .modeling.roi_heads import grit_roi_heads
+from .modeling.backbone import vit
+
+from .data.datasets import vg
+from .data.datasets import grit_coco
